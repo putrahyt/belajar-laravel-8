f2c471a8717a5df78e6192843c1f26e86bc7a5fc
putrahyt <putrahidayat629@gmail.com>
2026-03-12 15:31:33 +0700

    add catatan tutorial3

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <title>Jett Blog | About</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')    
     <h3>Halaman About</h3>
     <h5>{{ $nama }}</h5>
     <p>{{ $email }}</p>
     <img src="{{ asset('img/' . $image) }}" alt="{{ $nama }}" width="100">
-</body>
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'title' => 'Home'
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        'title' => 'About',
         'nama' => 'Putra Hidayat',
         'email' => 'putrahidayat629@gmail.com',
         'image' => 'pasfoto.jpg'
@@ -26,5 +29,57 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            'title' => 'Judul Pertama',
+            'slug' =>'judul-pertama',
+            'author' => 'Putra',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum iure voluptatibus reiciendis voluptas? Distinctio aperiam rem magnam, cum officia est assumenda ex dolorum alias quasi omnis! Fugiat incidunt accusamus enim nemo possimus nesciunt quis nisi. Hic, nesciunt provident et veritatis nostrum dicta laudantium ducimus atque vero dolores maiores repellendus, non dignissimos doloribus eos possimus perferendis nemo expedita rerum! Quia, nostrum quas. Quis consequuntur fugit sequi repellat est aut laborum odio. Quam ab suscipit totam nisi dignissimos sapiente dolores molestias, dicta, aspernatur iste autem alias voluptates tenetur rerum, officiis labore corrupti. Unde minus deserunt accusamus nostrum facilis aspernatur libero, dolores iste.'
+        ],
+        [
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'author' => 'Jett',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium voluptatibus, expedita harum neque quod totam quaerat ea aliquid earum dolorum similique minima quidem, dolorem, autem nisi. Animi itaque necessitatibus quisquam laboriosam possimus labore pariatur enim sapiente, praesentium nisi velit, aspernatur inventore numquam nesciunt architecto illum quod perferendis optio quis culpa nihil in quia et quos. Perferendis nesciunt blanditiis culpa quos mollitia sit qui exercitationem optio fuga animi! Explicabo sequi fugit alias incidunt autem deserunt quam distinctio aut, laborum sed provident sapiente natus nam ex a ducimus vel fugiat rem saepe cumque dicta modi eum inventore? Accusamus culpa odio blanditiis sint corrupti omnis eveniet animi possimus a at. Quod est harum dignissimos inventore deleniti voluptate facere, rem adipisci magnam maxime libero. Perspiciatis voluptate tempora facilis? Mollitia ea sed eum unde est magnam voluptas deleniti, corrupti animi temporibus, quibusdam eveniet corporis laborum facere et. Facilis magni, dolorum accusamus ipsa nisi aspernatur enim perferendis, doloribus est autem nesciunt deserunt. Nisi perferendis repellendus porro earum, dolorum incidunt quod beatae pariatur molestiae fuga qui nostrum enim recusandae sapiente blanditiis molestias praesentium quam, laudantium doloribus. Ipsum odio fugiat pariatur? Vero praesentium, quos corrupti voluptatum quibusdam mollitia sit aliquam voluptates dolorum iure officiis asperiores atque quaerat deleniti.'
+        ]
+    ];
+
+    return view('posts', [
+        'title' => 'Posts',
+        'posts' => $blog_posts
+    ]);
+});
+
+// Halaman single post
+Route::get('posts/{slug}', function($slug){
+    $blog_posts = [
+        [
+            'title' => 'Judul Pertama',
+            'slug' =>'judul-pertama',
+            'author' => 'Putra',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum iure voluptatibus reiciendis voluptas? Distinctio aperiam rem magnam, cum officia est assumenda ex dolorum alias quasi omnis! Fugiat incidunt accusamus enim nemo possimus nesciunt quis nisi. Hic, nesciunt provident et veritatis nostrum dicta laudantium ducimus atque vero dolores maiores repellendus, non dignissimos doloribus eos possimus perferendis nemo expedita rerum! Quia, nostrum quas. Quis consequuntur fugit sequi repellat est aut laborum odio. Quam ab suscipit totam nisi dignissimos sapiente dolores molestias, dicta, aspernatur iste autem alias voluptates tenetur rerum, officiis labore corrupti. Unde minus deserunt accusamus nostrum facilis aspernatur libero, dolores iste.'
+        ],
+        [
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'author' => 'Jett',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium voluptatibus, expedita harum neque quod totam quaerat ea aliquid earum dolorum similique minima quidem, dolorem, autem nisi. Animi itaque necessitatibus quisquam laboriosam possimus labore pariatur enim sapiente, praesentium nisi velit, aspernatur inventore numquam nesciunt architecto illum quod perferendis optio quis culpa nihil in quia et quos. Perferendis nesciunt blanditiis culpa quos mollitia sit qui exercitationem optio fuga animi! Explicabo sequi fugit alias incidunt autem deserunt quam distinctio aut, laborum sed provident sapiente natus nam ex a ducimus vel fugiat rem saepe cumque dicta modi eum inventore? Accusamus culpa odio blanditiis sint corrupti omnis eveniet animi possimus a at. Quod est harum dignissimos inventore deleniti voluptate facere, rem adipisci magnam maxime libero. Perspiciatis voluptate tempora facilis? Mollitia ea sed eum unde est magnam voluptas deleniti, corrupti animi temporibus, quibusdam eveniet corporis laborum facere et. Facilis magni, dolorum accusamus ipsa nisi aspernatur enim perferendis, doloribus est autem nesciunt deserunt. Nisi perferendis repellendus porro earum, dolorum incidunt quod beatae pariatur molestiae fuga qui nostrum enim recusandae sapiente blanditiis molestias praesentium quam, laudantium doloribus. Ipsum odio fugiat pariatur? Vero praesentium, quos corrupti voluptatum quibusdam mollitia sit aliquam voluptates dolorum iure officiis asperiores atque quaerat deleniti.'
+        ]
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post['slug'] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    if (empty($new_post)) {
+        abort(404);
+    }
+
+    return view('post', [
+        'title' => 'Single Post',
+        'post' => $new_post
+    ]);
 });
