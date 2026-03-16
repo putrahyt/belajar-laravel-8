e05cc04ccd80a324a3771332406cb93107e8a6d5
putrahyt <putrahidayat629@gmail.com>
2026-03-16 14:58:10 +0700

    add catatan tutorial8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+
+        User::create([
+            'name' => 'Putra',
+            'email' => 'putra@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Jett',
+            'email' => 'jett@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis neque repellendus perferendis, architecto suscipit impedit hic.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis neque repellendus perferendis, architecto suscipit impedit hic. Nostrum ad cupiditate distinctio beatae architecto porro consequatur accusamus eligendi dolor corrupti dolorum, pariatur ab, esse eos quisquam qui eaque commodi quae totam, facilis rem fugiat quod ipsum quasi. Distinctio, vitae vero praesentium eos vel maiores necessitatibus asperiores numquam architecto. Commodi consequuntur accusantium optio architecto quaerat unde temporibus neque esse sint, consectetur dolor ut quasi hic culpa? Commodi, fugit? Voluptatibus, incidunt ut iste, corporis voluptatum consequatur debitis blanditiis rerum error delectus exercitationem? Odio dicta, quis iure eveniet sit magni suscipit voluptatem sunt facilis, similique voluptates, totam sequi libero ipsam nobis consequatur cum odit asperiores aperiam illo sed cupiditate quibusdam. Vero pariatur ipsum atque sed rerum quaerat excepturi! Ducimus quisquam repudiandae, cumque nisi blanditiis commodi deleniti quia autem expedita provident unde praesentium vero vitae recusandae sapiente maiores ipsum incidunt consequatur itaque odit laudantium aperiam dolorum.',
+            'category_id' => 1,
+            'user_id' => 1 
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis neque repellendus perferendis, architecto suscipit impedit hic.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis neque repellendus perferendis, architecto suscipit impedit hic. Nostrum ad cupiditate distinctio beatae architecto porro consequatur accusamus eligendi dolor corrupti dolorum, pariatur ab, esse eos quisquam qui eaque commodi quae totam, facilis rem fugiat quod ipsum quasi. Distinctio, vitae vero praesentium eos vel maiores necessitatibus asperiores numquam architecto. Commodi consequuntur accusantium optio architecto quaerat unde temporibus neque esse sint, consectetur dolor ut quasi hic culpa? Commodi, fugit? Voluptatibus, incidunt ut iste, corporis voluptatum consequatur debitis blanditiis rerum error delectus exercitationem? Odio dicta, quis iure eveniet sit magni suscipit voluptatem sunt facilis, similique voluptates, totam sequi libero ipsam nobis consequatur cum odit asperiores aperiam illo sed cupiditate quibusdam. Vero pariatur ipsum atque sed rerum quaerat excepturi! Ducimus quisquam repudiandae, cumque nisi blanditiis commodi deleniti quia autem expedita provident unde praesentium vero vitae recusandae sapiente maiores ipsum incidunt consequatur itaque odit laudantium aperiam dolorum.',
+            'category_id' => 1,
+            'user_id' => 1 
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis neque repellendus perferendis, architecto suscipit impedit hic.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis neque repellendus perferendis, architecto suscipit impedit hic. Nostrum ad cupiditate distinctio beatae architecto porro consequatur accusamus eligendi dolor corrupti dolorum, pariatur ab, esse eos quisquam qui eaque commodi quae totam, facilis rem fugiat quod ipsum quasi. Distinctio, vitae vero praesentium eos vel maiores necessitatibus asperiores numquam architecto. Commodi consequuntur accusantium optio architecto quaerat unde temporibus neque esse sint, consectetur dolor ut quasi hic culpa? Commodi, fugit? Voluptatibus, incidunt ut iste, corporis voluptatum consequatur debitis blanditiis rerum error delectus exercitationem? Odio dicta, quis iure eveniet sit magni suscipit voluptatem sunt facilis, similique voluptates, totam sequi libero ipsam nobis consequatur cum odit asperiores aperiam illo sed cupiditate quibusdam. Vero pariatur ipsum atque sed rerum quaerat excepturi! Ducimus quisquam repudiandae, cumque nisi blanditiis commodi deleniti quia autem expedita provident unde praesentium vero vitae recusandae sapiente maiores ipsum incidunt consequatur itaque odit laudantium aperiam dolorum.',
+            'category_id' => 2,
+            'user_id' => 1 
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis neque repellendus perferendis, architecto suscipit impedit hic.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis neque repellendus perferendis, architecto suscipit impedit hic. Nostrum ad cupiditate distinctio beatae architecto porro consequatur accusamus eligendi dolor corrupti dolorum, pariatur ab, esse eos quisquam qui eaque commodi quae totam, facilis rem fugiat quod ipsum quasi. Distinctio, vitae vero praesentium eos vel maiores necessitatibus asperiores numquam architecto. Commodi consequuntur accusantium optio architecto quaerat unde temporibus neque esse sint, consectetur dolor ut quasi hic culpa? Commodi, fugit? Voluptatibus, incidunt ut iste, corporis voluptatum consequatur debitis blanditiis rerum error delectus exercitationem? Odio dicta, quis iure eveniet sit magni suscipit voluptatem sunt facilis, similique voluptates, totam sequi libero ipsam nobis consequatur cum odit asperiores aperiam illo sed cupiditate quibusdam. Vero pariatur ipsum atque sed rerum quaerat excepturi! Ducimus quisquam repudiandae, cumque nisi blanditiis commodi deleniti quia autem expedita provident unde praesentium vero vitae recusandae sapiente maiores ipsum incidunt consequatur itaque odit laudantium aperiam dolorum.',
+            'category_id' => 2,
+            'user_id' => 2 
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,11 +2,13 @@
 
 @section('container')
     <article>
-        <h2>{{ $post->title }}</h2>
-        <p>By. Putra Hidayat in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-        <h5>{{ $post->author }}</h5>
+        <h1  class="mb-5">{{ $post->title }}</h1>
+
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
         {!! $post->body !!}
+
+        <a href="/blog" class="d-block mt-3">Back to Posts</a>
     </article>
 
-    <a href="/blog">Back to Posts</a>
 @endsection
