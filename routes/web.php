d5e7831ed0aa974f96fd24250474813bc753339d
putrahyt <putrahidayat629@gmail.com>
2026-03-17 15:24:16 +0700

    add catatan tutorial10

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -45,16 +44,15 @@ Route::get('/categories', function() {
 });
 
 Route::get('/categories/{category:slug}', function(Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->post,
-        'category' => $category->name
+    return view('posts', [
+        'title' => "Post by Category : $category->name",
+        'posts' => $category->post->load('category', 'author'),
     ]);
 });
 
 Route::get('/author/{author:username}', function(User $author) {
     return view('posts', [
-        'title' => 'User Posts',
-        'posts' => $author->post,
+        'title' => "Post by Author : $author->name",
+        'posts' => $author->post->load('category', 'author'),
     ]);
 });
