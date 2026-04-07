0c19531795a870eaabfb133f860f860fcdb7bedd
putrahyt <putrahidayat629@gmail.com>
2026-04-07 14:58:16 +0700

    add catatan tutorial14

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -11,5 +13,26 @@ class RegisterController extends Controller
         return view('register.index', [
             'title' => 'Register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:3|max:255|unique:users',
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // $validated['password'] = bcrypt($validated['password']);
+        $validated['password'] = Hash::make($validated['password']);
+
+        User::create($validated);
+        
+        // Kirim flash bisa pake cara ini
+        // $request->session()->flash('success', 'Registration successfull! Please login');
+        
+        // atau langsung saat redirect
+        return redirect('/login')->with('success', 'Registration successfull! Please login');
     }
 }
